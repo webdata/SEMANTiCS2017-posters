35aabfa19e1471486dfc3d25814ead85467bf510
Marvin Hofer <hofer@informatik.uni-leipzig.de>
2017-09-22 13:44:40 +0200

    paper 113 and 152 final

--- test.sh
+++ test.sh
@@ -6,6 +6,5 @@ ls papers_final | while read -r a;
 do
 	if [ ! -e "papers_final/$a/$a.pdf" ]; then
 		echo $a
-		../wkhtmltopdf -L 20mm -R 20mm -B 20mm -T 20mm papers_final/$a/index.html papers_final/$a/$a.pdf
 	fi
 done
